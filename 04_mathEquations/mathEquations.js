2b5bbebe4fac2ea03b4601ddc96eacf59341df32
soyoung23-debug <192168844+soyoung23-debug@users.noreply.github.com>
2025-11-15 22:29:36 +0800

    Replace string variables with mathematical expressions

--- 04_mathEquations/mathEquations.js
+++ 04_mathEquations/mathEquations.js
@@ -1,9 +1,22 @@
+//Replace the strings to the right of the = with the expression they describe. Do not manually enter the answers to the equation.
+
+// Currently, the variables are strings. They need to be math equations instead.
+
+// To help you, the first variable has been completed for you:
+
+// const a = 1 - 1 // Freebie!!! This is the answer to "one minus one"
+// The following is an example of the wrong answer:
+
+// const a = 0 // WRONG!!! You need to write out the equation, NOT put the result of the equation directly!
+// Feel free to run the test suite right now to see the first test pass, then go from there. Good luck!
+
+
 const a = 1 - 1 // Freebie!!! This is the answer to "one minus one"
-const b = "one plus eight"
-const c = "22 times three"
-const d = "the *remainder* of 5/4"
-const e = "the variable 'b' minus 17"
-const f = "the sum of the previous five variables"
+const b = 1 + 8
+const c = 22 * 3
+const d = 5 % 3
+const e = 'b - 17'
+const f = 'a + b + c + d +e'
 
 // Do not edit below this line
 module.exports = {a, b, c, d, e, f}
