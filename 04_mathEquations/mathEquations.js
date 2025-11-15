6c405e3fd9617748d4d0f1280c11f114b67dc429
soyoung23-debug <192168844+soyoung23-debug@users.noreply.github.com>
2025-11-15 22:30:07 +0800

    Fix variable assignments in mathEquations.js

--- 04_mathEquations/mathEquations.js
+++ 04_mathEquations/mathEquations.js
@@ -15,8 +15,8 @@ const a = 1 - 1 // Freebie!!! This is the answer to "one minus one"
 const b = 1 + 8
 const c = 22 * 3
 const d = 5 % 3
-const e = 'b - 17'
-const f = 'a + b + c + d +e'
+const e = b - 17
+const f = a + b + c + d +e
 
 // Do not edit below this line
 module.exports = {a, b, c, d, e, f}
