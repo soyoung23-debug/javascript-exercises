5ed8eaf935702adcffd277de020b2e9cbe5b3f15
soyoung23-debug <192168844+soyoung23-debug@users.noreply.github.com>
2025-11-15 22:26:23 +0800

    Update numberChecker.js

--- 03_numberChecker/numberChecker.js
+++ 03_numberChecker/numberChecker.js
@@ -1,5 +1,7 @@
+//Modify the code so it returns true when the number is greater than or equal to 10, and false if it is less than 10.
+
 function numberChecker(number) {
-  if (number === 6) {
+  if (number >=10) {
     return true;
   } else {
     return false;
